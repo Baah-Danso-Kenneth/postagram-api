858a170d0cd94c4b787425053cd2389799d4a333
Kenneth <kwadwobaahdanso@gmail.com>
2023-12-13 02:04:30 +0000

    Initialize git API Project

---
+++ core/user/models.py
@@ -0,0 +1,79 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, PermissionsMixin, AbstractBaseUser
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
+from core.abstract.models import AbstractManager, AbstractModel
+
+def user_directory_path(instance, filename):
+    return "user_{0}/{1}".format(instance.public_id, filename)
+
+class UserManager(BaseUserManager, AbstractManager):
+        
+    def create_user(self,username, email, password=None, **kwargs):
+        if username is None:
+            raise TypeError('User must have a username')
+        if email is None:
+            raise TypeError('User must have email account')
+        if password is None:
+            raise TypeError('User must have a password')
+        
+        user = self.model(username=username, email= self.normalize_email(email), **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+
+    def create_superuser(self,username, email, password, **kwargs):
+        if password is None:
+            raise TypeError('Superuser must have a password')
+        if email is None:
+            raise TypeError('Superuser must have email')
+        if username is None:
+            raise TypeError('Superuser must have a username')
+        user = self.create_user(username, email, password, **kwargs)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+        
+    
+class User(AbstractModel,AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    bio = models.TextField(null=True)
+    avatar = models.ImageField(null=True, blank=True, upload_to=user_directory_path)
+    posts_liked = models.ManyToManyField(
+        "core_post.Post",
+        related_name='liked_by'
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    objects = UserManager()
+
+    def __str__(self):
+        return f"{self.email}"
+    
+
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def like(self, post):
+        return self.posts_liked.add(post)
+    
+    def remove_like(self, post):
+        return self.posts_liked.remove(post)
+    
+    def has_liked(self,post):
+        return self.posts_liked.filter(pk=post.pk).exists()
+    
+
